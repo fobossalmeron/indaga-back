78167ac52e056e77982481fc5394090f26daf8d9
Rodrigo Salmeron <rodrigosalmeron@192.168.1.2>
2024-09-11 20:39:44 -0600

    texts changed

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,5 +1,22 @@
 export default {
   config: {
+    auth: {
+      logo: "/indaga_icon.svg",
+    },
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "¡Welcome to Indaga!",
+        "Auth.form.welcome.subtitle": "Login to your account",
+        "app.components.LeftMenu.navbrand.title": "Indaga",
+        "app.components.LeftMenu.navbrand.workplace": "Web Dashboard",
+      },
+    },
+    theme: {
+      colors: {
+        // primary: '#000',
+        // secondary: '#fff',
+      },
+    },
     locales: [
       // 'ar',
       // 'fr',
